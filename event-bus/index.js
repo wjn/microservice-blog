903d512db6d293f2fad13c19c62981418419c003
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-08-29 10:24:55 -0500

    event bus skeleton

---
+++ event-bus/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+app.use(bodyParser.json());
+
+const urlPostService = 'http://localhost:4000';
+const urlCommentsService = 'http://localhost:4001';
+const urlQueryService = 'http://localhost:4002';
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  axios.post(`${urlCommentsService}/events`, event);
+  axios.post(`${urlPostService}/events`, event);
+  axios.post(`${urlQueryService}/events`, event);
+
+  res.status(200).send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Event Bus listening on port 4005');
+});
